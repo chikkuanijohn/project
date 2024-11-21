f3a068ee0795c2977bc65b23340b43ae7a48e805
chikkuanijohn <chikkuanijohn1@gmail.com>
2024-11-21 14:44:36 +0530

    new file

--- project1/app/urls.py
+++ project1/app/urls.py
@@ -9,6 +9,7 @@ urlpatterns=[
     path('addproduct',views.add_product),
     path('edit_product/<pid>', views.add_product),  
     path('delete_product/<pid>',views.delete_product),
+    
     path('register',views.register), 
     path('user_home',views.user_home),
     path('product_dtls/<pid>',views.view_product),
